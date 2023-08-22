76bdfacdc10646e707e1223ea53de5f5ddd1b5d4
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-22 11:38:27 -0400

    Fix join query

--- app.py
+++ app.py
@@ -2,29 +2,30 @@ import sqlite3
 
 from flask import Flask, jsonify, render_template
 
-
 app = Flask(__name__)
-
 conn = sqlite3.connect("./instance/database.sqlite", check_same_thread=False)
 
 
 def get_authors():
     cur = conn.cursor()
-    sql = "SELECT * FROM Author"
-    cur.execute(sql)
-    x = cur.fetchall()
-    print(x)
-    return x
+    cur.execute("SELECT * FROM Author")
+    cur.close()
+    return cur.fetchall()
 
 
-def get_books(author_id):
+def get_books_1(author_id):
     cur = conn.cursor()
-    sql = f"SELECT * FROM Book WHERE author_id = {author_id}"
-    cur.execute(sql)
-    x = cur.fetchall()
-    print(x)
-    return x
+    cur.execute(f"SELECT * FROM Book WHERE author_id = {author_id}")
+    cur.close()
+    return cur.fetchall()
 
+
+def get_books_2():
+    cur = conn.cursor()
+    sql = f"SELECT * FROM Book JOIN Author ON Book.author_id = Author.id"
+    cur.execute(sql)
+    cur.close()
+    return cur.fetchall()
 
 
 @app.get("/")
@@ -38,20 +39,12 @@ async def get_data():
     output = []
     for author in authors:
         author_id = author[0]
-        books = get_books(author_id)
+        books = get_books_1(author_id)
         output.append(books)
     return jsonify(output)
 
 
-def query_all():
-    cur = conn.cursor()
-    sql = f"SELECT * FROM Author, Book JOIN Author ON Book.author_id = Author.id"
-    cur.execute(sql)
-    x = cur.fetchall()
-    print(x)
-    return x
-
-
 @app.get("/get-data-2")
 async def get_data2():
-    return jsonify(query_all())
+    books = get_books_2()
+    return jsonify(books)
