a82f0fcbfdac9564d671f7ec968a00aa3671a042
mhmh4 <86537561+mhmh4@users.noreply.github.com>
2023-08-22 11:08:09 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,39 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
+conn = sqlite3.connect("./instance/database.sqlite", check_same_thread=False)
+
+
+def get_authors():
+    cur = conn.cursor()
+    sql = "SELECT * FROM Author"
+    cur.execute(sql)
+    x = cur.fetchall()
+    print(x)
+    return x
+
+
+def get_books(author_id):
+    cur = conn.cursor()
+    sql = f"SELECT * FROM Book WHERE author_id = {author_id}"
+    cur.execute(sql)
+    x = cur.fetchall()
+    print(x)
+    return x
+
+
+
 @app.get("/")
 def index():
     return render_template("index.html")
 
+
 @app.get("/get-data")
 async def get_data():
-    return jsonify({"test": 123})
+    authors = get_authors()
+    output = []
+    for author in authors:
+        author_id = author[0]
+        books = get_books(author_id)
+        output.append(books)
+    return jsonify(output)
